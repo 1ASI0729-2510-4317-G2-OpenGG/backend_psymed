1d6c68a213eb7515ac19f507ec7abb3221547d4d
feg06 <u202213468@upc.edu.pe>
2025-06-22 10:56:15 -0500

    feat: add remaining comments for medic sections

--- src/main/java/com/openGG/psymed/plattform/Medical_Management/medic_sections/interfaces/rest/transform/CreateSectionCommandFromResourceAssembler.java
+++ src/main/java/com/openGG/psymed/plattform/Medical_Management/medic_sections/interfaces/rest/transform/CreateSectionCommandFromResourceAssembler.java
@@ -3,7 +3,20 @@ package com.openGG.psymed.plattform.Medical_Management.medic_sections.interfaces
 import com.openGG.psymed.plattform.Medical_Management.medic_sections.domain.model.commands.CreateSectionCommand;
 import com.openGG.psymed.plattform.Medical_Management.medic_sections.interfaces.rest.resources.CreateSectionResource;
 
+/**
+ * Assembler class responsible for transforming a {@link CreateSectionResource}
+ * into a {@link CreateSectionCommand} used by the application layer.
+ */
+
 public class CreateSectionCommandFromResourceAssembler {
+
+    /**
+     * Converts a {@link CreateSectionResource} into a {@link CreateSectionCommand}.
+     *
+     * @param resource the resource received from the REST client
+     * @return a command object with the section's creation data
+     */
+
     public static CreateSectionCommand toCommandFromResource(CreateSectionResource resource) {
         return new CreateSectionCommand(
                 resource.patientId(),
--- src/main/java/com/openGG/psymed/plattform/Medical_Management/medic_sections/interfaces/rest/transform/SectionResourceFromEntityAssembler.java
+++ src/main/java/com/openGG/psymed/plattform/Medical_Management/medic_sections/interfaces/rest/transform/SectionResourceFromEntityAssembler.java
@@ -3,7 +3,20 @@ package com.openGG.psymed.plattform.Medical_Management.medic_sections.interfaces
 import com.openGG.psymed.plattform.Medical_Management.medic_sections.domain.model.entities.Section;
 import com.openGG.psymed.plattform.Medical_Management.medic_sections.interfaces.rest.resources.SectionResource;
 
+/**
+ * Assembler class responsible for transforming a {@link Section} domain entity
+ * into a {@link SectionResource} used in REST responses.
+ */
+
 public class SectionResourceFromEntityAssembler {
+
+    /**
+     * Converts a {@link Section} entity into a {@link SectionResource}.
+     *
+     * @param section the domain entity representing a medical section
+     * @return a resource object formatted for REST responses
+     */
+
     public static SectionResource toResourceFromEntity(Section section) {
         return new SectionResource(
                 section.getId(),
